33d2a8c18aa44f51b4d54666ec0272432d3e4f9b
jasonjas <jason@jasonsvatos.net>
2016-03-13 20:29:11 -0500

    Added files via upload

---
+++ IIS-Commands.ps1
@@ -0,0 +1,50 @@
+﻿# IIS Web administration functions
+
+Import-Module WebAdministration
+
+function Backup-Config {
+    <#
+        .SYNOPSIS
+            See: Get-Help Backup-WebConfiguration -Full
+    #>
+    param($Name)
+    
+    Backup-WebConfiguration -Name $Name
+}
+
+function Restore-Config {
+    <#
+        .SYNOPSIS
+            See: Get-Help Restore-WebConfiguration -Full
+    #>
+    param($Name)
+
+    Restore-WebConfiguration -Name $Name
+}
+
+function List-Backups {
+    <#
+        .SYNOPSIS
+            List backups on the server
+    #>
+
+    Get-WebConfigurationBackup -Name *
+}
+
+function List-AppPools {
+    <#
+        .SYNOPSIS
+            List all application pools
+    #>
+
+    Get-ChildItem -Path IIS:\AppPools
+}
+
+function List-Sites {
+    <#
+        .SYNOPSIS
+            List all web sites
+    #>
+    
+    Get-ChildItem -Path IIS:\Sites
+}
