5615c14243e61449810ba7ef741d99c99c4b3459
jasonjas <jason@jasonsvatos.net>
2018-06-30 08:24:24 -0500

    Update AWS-Backup.ps1

--- AWS-Backup.ps1
+++ AWS-Backup.ps1
@@ -1,70 +1,33 @@
-﻿Import-Module awspowershell
+Import-Module awspowershell
 
 function AWS-Backup {
-    <#
-        .SYNOPSIS
-            Backup files to S3 based on locations in a text file
-            Can back up individual files or whole directories
-            Requires AWS Credentials to be configured prior to running script.
-            See https://docs.aws.amazon.com/powershell/latest/reference/items/Set-AWSCredentials.html 
-            
-        .DESCRIPTION
-            Will store file hashes in a text file and compare each time the file is backed up.
-            If any changes in directory, the whole directory will be re-uploaded. 
-            Will have to specify full location to each file if you want to upload manually instead. 
-
-        .NOTES
-            Requires AWS Powershell cmdlets
-            See https://aws.amazon.com/powershell/
-            Requires AWS Credentials to be configured prior to running script.
-            See https://docs.aws.amazon.com/powershell/latest/reference/items/Set-AWSCredentials.html
-
-        .PARAMETER AwsCredentials
-            Saved AWS Credential
-
-        .PARAMETER region
-            Region to upload files to
-
-        .PARAMETER ConfigFile
-            Path to list of hashes for existing files, or path to store the hashes for files. 
-
-        .PARAMETER bucketName
-            Name of bucket in S3 to upload files to
-
-        .PARAMETER backupFilesList 
-            Path to location of files/directories to backup
-
-        .PARAMETER FullRefresh
-            Removes ConfigFile hash list and re-uploads all files/directories whether they have been uploaded previously or not
-    #>
     param (
         [parameter(mandatory=$false,
                    Position=0)]
-        [String]$AwsCredentials="",
-        [String]$region = "",
-        [String]$ConfigFile = "",
-        [String]$bucketName = "",
-        [String]$backupFilesList = "",
-        [Switch]$FullRefresh
+        [String]$AwsCredentials="jasonjas",
+        [String]$region = "us-east-1",
+        [String]$ConfigFile = "F:\UserFiles\BackupFileHash.config",
+        [String]$bucketName = "jasonsvatos-backup",
+        [String]$backupFilesList = "C:\Users\jason\Desktop\BackupFilesS3.txt"
     )
-
-    if ($FullRefresh) {
-        if ([System.IO.File]::Exists($ConfigFile)) {Remove-Item $ConfigFile}
-    }
 
     Set-AWSCredentials -StoredCredentials $AwsCredentials
     Set-DefaultAWSRegion $region
     # special characters that do not show up correctly in the text file
     # Will be used to replace characters later
+    $SpecChars = '!', '£', '%', '&', '^', '*', '@', '=', '+', '¬', '`', '<', '>', '?', ';', '#', '~', '®', 'é', '–', "'", '"', "’", "[", "]"
+    $remspecchars = [string]::join('|', ($SpecChars | % {[regex]::escape($_)}))
     $backupFiles = gc $backupFilesList
 
     function BackupFiles() {
+        #$backupfiles = gc "$desktop\BackupFilesS3.txt"
         [int]$folderChange = 0
 
         # backup files
         foreach ($file in $backupfiles) {
             # check for comment characters and ignore
             if ($file.Trim().StartsWith("#")) {}
+            
             elseif ((Get-Item $file) -is [System.IO.DirectoryInfo]) {
                 # is a directory
                 # get the directory name and set as the prefix/folder to store under root bucket
@@ -74,23 +37,25 @@ function AWS-Backup {
                     # check if any items have been changed in folder, if not, skip the upload of the folder
                     if ((Get-Item $item.FullName) -is [System.IO.DirectoryInfo]) {} #do nothing
                     elseif ((Set-FileHash -Path $item.FullName) -ne $null) {
+                        $s3path = $item.FullName.Substring($item.FullName.IndexOf("$keyprefix"))
+                        Write-S3Object -BucketName $bucketName -File $item.FullName -Key $s3path
+                        Write-Output "Uploading $($item.FullName)"
                         $folderChange = 1
                     }
                 }
-                if ($folderChange -ne 0) {
-                    Write-Output "Uploading folder $file"
-                    Write-S3Object -BucketName $bucketName -Folder $file -KeyPrefix $keyprefix -Recurse
+                if ($folderChange -eq 0) {
+                    Write-Output "No change for directory $file"
                 }
-                else {Write-Output "No change for $file"}
             }
+
             else 
             {
                 # is a file
                 if ((Set-FileHash -Path $file) -ne $null) {
-                    Write-Output "Uploading file $file"
+                    Write-Host "Uploading file $file"
                     Write-S3Object -BucketName $bucketName -File $file
                 }
-                else {Write-Output "Skipping $file"}
+                else {Write-Host "Skipping $file"}
             }
         }
 
@@ -106,6 +71,9 @@ function AWS-Backup {
 
             .PARAMETER Path
                 Path to the file to set the hash for
+
+            .PARAMETER ConfigFile
+                Path to location of config file containing hashes
         #>
 
         param(
@@ -115,9 +83,6 @@ function AWS-Backup {
     
         # set change if hash CSV file is updated
         [int]$change = 0
-
-        $SpecChars = '!', '£', '%', '&', '^', '*', '@', '=', '+', '¬', '`', '<', '>', '?', ';', '#', '~', '®', 'é', '–', "'", '"', "’", "[", "]"
-        $remspecchars = [string]::join('|', ($SpecChars | % {[regex]::escape($_)}))
 
         # Check if config file exists, if not - create it
         if (-not (Test-Path $ConfigFile)) {
@@ -171,6 +136,9 @@ function AWS-Backup {
             .PARAMETER Path
                 Path of file to search for hash on
 
+            .PARAMETER ConfigFile
+                Path to location of config file containing hashes
+
             .PARAMETER List
                 List hashes instead of searching for a single object
 
@@ -204,3 +172,5 @@ function AWS-Backup {
 
     BackupFiles
 }
+
+AWS-Backup
